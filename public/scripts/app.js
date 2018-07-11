05c5584c946e8e7ce480791ef2738a87a29e71ef
Vicente Noriega <vicente.noriega@corp.pounceconsulting.com>
2018-07-11 16:39:19 -0500

    SE3; Hello World; 10. Exploring JSX.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,61 @@ console.log("app.js is running ...");
 
 // jsx - javascript xml
 var template = React.createElement(
-  "h1",
-  { className: "myClass" },
-  "This is JSX from src/app.js"
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Indecision App"
+	),
+	React.createElement(
+		"p",
+		null,
+		"This is some info"
+	),
+	React.createElement(
+		"ol",
+		null,
+		React.createElement(
+			"li",
+			null,
+			"Item one"
+		),
+		React.createElement(
+			"li",
+			null,
+			"Item two"
+		)
+	)
 );
+
+// Create a templateTwo var JSX expression
+//	div
+//		h1 -> Your name
+//		p -> Age: 27 ...
+//		p -> Location: Guadalajara Jal
+// Render TemplateTwo instead of template
+
+var templateTwo = React.createElement(
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Vicente Noriega"
+	),
+	React.createElement(
+		"p",
+		null,
+		"Age: 27"
+	),
+	React.createElement(
+		"p",
+		null,
+		"Location: Guadalajara Jalisco, M\xE9xico"
+	)
+);
+
 var appRoute = document.getElementById("app");
-ReactDOM.render(template, appRoute);
+
+ReactDOM.render(templateTwo, appRoute);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,32 @@
 console.log("app.js is running ...");
 
 // jsx - javascript xml
-var template = <h1 className="myClass">This is JSX from src/app.js</h1>
+var template = ( 
+	<div>
+		<h1>Indecision App</h1>
+		<p>This is some info</p>
+		<ol>
+			<li>Item one</li>
+			<li>Item two</li>
+		</ol>
+	</div>
+);
+
+// Create a templateTwo var JSX expression
+//	div
+//		h1 -> Your name
+//		p -> Age: 27 ...
+//		p -> Location: Guadalajara Jal
+// Render TemplateTwo instead of template
+
+var templateTwo = (
+	<div>
+		<h1>Vicente Noriega</h1>
+		<p>Age: 27</p>
+		<p>Location: Guadalajara Jalisco, México</p>
+	</div>
+);
+
 var appRoute = document.getElementById("app");
-ReactDOM.render(template, appRoute);
+
+ReactDOM.render(templateTwo, appRoute);
