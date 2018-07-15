e267a09000c8a030f80e3661a3fcbe83b711784e
Vicente Noriega <vicente.noriega.sc@gmail.com>
2018-07-14 21:46:19 -0500

    SE3; Hello World; 12. Conditional Rendering in JSX.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,17 @@
 
 console.log("app.js is running ...");
 
-// create app object title/subtitle
-// use title/subtitle in the template
-// render template
+// if statement
+// ternary operators
+// logical and operator
+
+// only render the subtitle (and p tag) if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 "Here are your options" : "No options"
+
 var app = {
 	title: 'Indecision App',
-	subtitle: 'Put your life in the hands of a computer'
+	subtitle: 'Put your life in the hands of a computer',
+	options: ['One', 'Two']
 };
 // jsx - javascript xml
 var template = React.createElement(
@@ -18,10 +23,15 @@ var template = React.createElement(
 		null,
 		app.title
 	),
-	React.createElement(
+	app.subtitle && React.createElement(
 		'p',
 		null,
 		app.subtitle
+	),
+	React.createElement(
+		'p',
+		null,
+		app.options.length > 0 ? "Here are your options" : "No options"
 	),
 	React.createElement(
 		'ol',
@@ -50,29 +60,30 @@ var user = {
 	age: 27,
 	location: 'Guadalajara Jalisco, México'
 };
-// var userName = 'Vicente Noriega';
-// var userAge = 27;
-// var userLocation = 'Guadalajara Jalisco, México.';
+function getLocation(location) {
+	if (location) return React.createElement(
+		'p',
+		null,
+		'Location: ',
+		location
+	);
+	return undefined;
+}
 var templateTwo = React.createElement(
 	'div',
 	null,
 	React.createElement(
 		'h1',
 		null,
-		user.name
+		user.name ? user.name : 'Anonymous'
 	),
-	React.createElement(
+	user.age && user.age >= 18 && React.createElement(
 		'p',
 		null,
 		'Age: ',
 		user.age
 	),
-	React.createElement(
-		'p',
-		null,
-		'Location: ',
-		user.location
-	)
+	getLocation(user.location)
 );
 
 var appRoute = document.getElementById("app");
--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 console.log("app.js is running ...");
 
-// create app object title/subtitle
-// use title/subtitle in the template
-// render template
+// if statement
+// ternary operators
+// logical and operator
+
+// only render the subtitle (and p tag) if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 "Here are your options" : "No options"
+
 var app = {
 	title: 'Indecision App',
-	subtitle: 'Put your life in the hands of a computer'
+	subtitle: 'Put your life in the hands of a computer',
+	options: ['One', 'Two']
 };
 // jsx - javascript xml
 var template = ( 
 	<div>
 		<h1>{app.title}</h1>
-		<p>{app.subtitle}</p>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		<p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
 		<ol>
 			<li>Item one</li>
 			<li>Item two</li>
@@ -30,14 +36,16 @@ var user = {
 	age: 27,
 	location: 'Guadalajara Jalisco, México'
 };
-// var userName = 'Vicente Noriega';
-// var userAge = 27;
-// var userLocation = 'Guadalajara Jalisco, México.';
+function getLocation(location) {
+	if(location)
+		return <p>Location: {location}</p>;
+	return undefined;
+}
 var templateTwo = (
 	<div>
-		<h1>{user.name}</h1>
-		<p>Age: {user.age}</p>
-		<p>Location: {user.location}</p>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
+		{(user.age && user.age >= 18 ) && <p>Age: {user.age}</p>}
+		{getLocation(user.location)}
 	</div>
 );
 
