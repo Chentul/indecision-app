496f5559c0c78192cee85cabe2676314efc98c00
Vicente Noriega <vicente.noriega.sc@gmail.com>
2018-07-16 21:58:08 -0500

    SE4; React Components; 35. Build it: Adding State to VisibilityToggle.

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,4 +1,4 @@
-let visibility = false;
+/*let visibility = false;
 
 const toggleVisibility = () => {
 	visibility = !visibility;
@@ -23,4 +23,40 @@ const render = () => {
 	ReactDOM.render(jsx, document.getElementById('app'));
 };
 
-render();
+render();*/
+
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+		this.state = {
+			visibility: false
+		};
+	}
+	handleToggleVisibility() {
+		this.setState((prevState) => {
+			return {
+				visibility : !prevState.visibility
+			}
+		})
+	}
+	render() {
+		return(
+			<div>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.handleToggleVisibility}>
+					{
+						this.state.visibility ? 'Hide details' : 'Show details'
+					}
+				</button>
+				{this.state.visibility && (
+					<div>
+						<p>Hey. Theses are some details you can now see!</p>
+					</div>
+				)}
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
