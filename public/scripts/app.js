0f00c966bdfcd3e85211a9befbc6dc6d500a209a
Vicente Noriega <vicente.noriega@pouncecorp.com>
2018-07-18 13:51:48 -0500

    SE5; Stateless Functional Components; 46. Saving and Loading the Count.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,7 +21,7 @@ var IndecisionApp = function (_React$Component) {
 		_this.handleAddOption = _this.handleAddOption.bind(_this);
 		_this.handleDeleteOption = _this.handleDeleteOption.bind(_this);
 		_this.state = {
-			options: props.options
+			options: []
 		};
 		return _this;
 	}
@@ -116,11 +116,9 @@ var IndecisionApp = function (_React$Component) {
 	return IndecisionApp;
 }(React.Component);
 
-IndecisionApp.defaultProps = {
-	options: []
-};
-
 // this kind of component doesn't have the methods of the lyfecycle of react, but are more fast
+
+
 var Header = function Header(props) {
 	return React.createElement(
 		'div',
