8a23547b0f950393c81aaf86e1f4408f38f086a1
Vicente Noriega <vicente.noriega@pouncecorp.com>
2018-07-23 17:00:20 -0500

    SE8; Styling React; 73. Styling React-Modal.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
 		// allows you to close the box touching the box or clicking esc
 		onRequestClose={props.handleClearSelectedOption} 			
 		contentLabel="Selected Option"
+		closeTimeoutMS={200}
+		className="modal"
 	>
-		<h3>Selected Option</h3>
-		{props.selectedOption && <p>{props.selectedOption}</p>}
-		<button onClick={props.handleClearSelectedOption}>Okay</button>
+		<h3 className="modal__title">Selected Option</h3>
+		{props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+		<button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
 	</Modal>
 );
 
